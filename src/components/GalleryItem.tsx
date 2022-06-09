9353e58cf4e50e904ccf01465147b6ef3260592c
JeTSkY1h <jetsky1h@googlemail.com>
2022-06-09 19:12:16 +0200

    added log button

--- src/components/GalleryItem.tsx
+++ src/components/GalleryItem.tsx
@@ -1,5 +1,6 @@
 import "./GalleryItem.css";
-import {Char} from "../service/models"
+import {Char} from "../service/models";
+import {VscDebugConsole} from "react-icons/vsc";
 
 interface GalleryItemProps {
     char: Char;
@@ -7,15 +8,21 @@ interface GalleryItemProps {
 
 export default function GalleryItem(props: GalleryItemProps){
         let char = props.char
+
+        const handleClick = () => {
+            console.log(char);
+        }
+
         return (
             <div className="char card">
                 <img src={char.image} alt={`${char.name}`}/>
-                
-                <h3 className="char-name">{props.char.name}</h3>
+                <button onClick={handleClick} className="log-btn"><VscDebugConsole size={32}/></button>
+                <h3 className="char-name">{char.name}</h3>
                 <div className="char-info">
                     <p className="char-status"><span className="bold">{props.char.status}</span></p>
-                    <p className="char-location">{props.char.location.name}</p>
+                    <p className="char-location">{char.location.name}</p>
                 </div>
+                
             </div>
         )
 }
