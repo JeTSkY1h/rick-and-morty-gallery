8d4947688a7d62793847300f377510d37c31590b
JeTSkY1h <jetsky1h@googlemail.com>
2022-06-10 10:13:20 +0200

    Refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,14 @@
-import { useEffect, useState } from 'react';
 import './App.css';
 import Gallery from './components/Gallery';
-import Nav from './components/Nav';
-import {Response} from "./service/models";
-import { getAllChars } from "./service/apiService";
+
 
 
 function App() {
 
-  const [response, setResponse] = useState<Response>();
-  const [searchVal, setSearchval] = useState<string>(" ");
-
-  useEffect(()=>{
-    getAllChars(" ").then(data => {
-      setResponse(data);
-      console.log(data);
-      
-    })
-  },[])
-
-  function handleSearchChange(newVal: string){
-    setSearchval(newVal)
-    getAllChars(newVal).then(res => {
-      setResponse(res);
-      console.log(res);
-      
-    })
-  }
-
   return (
     <div className="App">
-      <header className="App-header">
-        <Nav  search={{onChange: handleSearchChange, setChars: setResponse, value: searchVal}}/>
-      </header>
       <main>
-        { response && <Gallery res={response} setResponse={setResponse}/>}
+        <Gallery/>
       </main>
       <footer>
 
--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -3,36 +3,50 @@ import  GalleryItem from "./GalleryItem";
 import {Response} from "../service/models"
 import {getNextPage, getPrevPage} from "../service/apiService"
 import {FaChevronLeft, FaChevronRight} from "react-icons/fa"
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
+import {getAllChars} from "../service/apiService";
+import Searchbar from "./Searchbar";
 
-interface GalleryProps {
-    res: Response,
-    setResponse: Function
-}
 
-export default function Gallery(props: GalleryProps){
+
+
+export default function Gallery(){
+
+    const [res, setRes] = useState<Response>();
+    const [search, setSearch] = useState(" ");
     
+    useEffect(()=>{
+        getAllChars(search).then(data => setRes(data))
+    })
+
+
     const nextPage = () => {
-        getNextPage(props.res).then(data => {
-            props.setResponse(data)
+        getNextPage(res!).then(data => {
+            setRes(data)
         })
     }
 
     const prevPage = () => {
-        getPrevPage(props.res).then(data=>{
-            props.setResponse(data);
+        getPrevPage(res!).then(data=>{
+            setRes(data);
         })
     }
 
+    const handleSearchChange = (newBal: string) => {
+        console.log(newBal)
+        setSearch(newBal)
+    }
+
     return (
-        <>
+        <>  
+            <Searchbar onChange={handleSearchChange} setChars={setRes} value={search}/>
             <div className="flex">
-                {props.res.results.map(char=>{
+                { res && res.results.map(char=>{
                    return <GalleryItem key={Math.random()*Date.now()} char={char}></GalleryItem>
                 })}
             </div>
             <div className="pagination">
-            {props.res.info.prev && <button onClick={prevPage}><FaChevronLeft/></button>} {props.res.info.pages} <button onClick={nextPage}><FaChevronRight/></button>
+            {res && res.info.prev && <button onClick={prevPage}><FaChevronLeft/></button>}  <button onClick={nextPage}><FaChevronRight/></button>
             </div>
         </>
     )
